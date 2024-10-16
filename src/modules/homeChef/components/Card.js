03de4b0ae55f1df91c63be36cbac40beb2dacede
Harsha Bhondwe <150532837+Harsha-0320@users.noreply.github.com>
2024-10-17 01:28:26 +0530

    Updates

--- src/modules/homeChef/components/Card.js
+++ src/modules/homeChef/components/Card.js
@@ -6,30 +6,30 @@ const Card = ({ cardInfo }) => {
   const buttonBgColor = buttonStatus === "View" ? "bg-red-500" : "bg-green-600";
 
   return (
-    <div className="bg-white text-sm  py-2 px-4 justify-center items-center md:px-5 md:py-3 rounded-xl grid grid-cols-5 md:gap-5 md:font-medium mb-3 lg:text-lg shadow-md">
-    <p>{id}</p>
-    <div className="flex gap-2 col-span-2">
-      <div>
-        <div className="bg-green-600 w-10 h-10 rounded-full flex justify-center items-center text-white font-bold shadow-lg">
-          {userName.charAt(0).toUpperCase()}
+    <div className="bg-white  text-sm py-4  sm:py-2 sm:px-4 justify-center items-center  rounded-xl grid grid-cols-5 md:gap-5 md:font-medium mb-3 lg:text-lg shadow-md">
+      <p className="px-1.5">{id}</p>
+      <div className="flex gap-2 col-span-2">
+        <div>
+          <div className="bg-green-600 w-7 h-7 sm:w-10 sm:h-10 rounded-full flex justify-center items-center text-white font-bold shadow-lg">
+            {userName.charAt(0).toUpperCase()}
+          </div>
+        </div>
+        <div>
+          <p>{userName}</p>
+          <p className="text-xs">{userAdd}</p>
         </div>
       </div>
+      <p className="flex gap-1 items-center">
+        {amount} <MdCurrencyRupee />
+      </p>
       <div>
-        <p>{userName}</p>
-        <p className="text-xs">{userAdd}</p>
+        <button
+          className={`${buttonBgColor} px-2.5 py-1.5 sm:px-4 sm:py-2 rounded-full text-white font-bold shadow-md`}
+        >
+          {buttonStatus}
+        </button>
       </div>
     </div>
-    <p className="flex gap-1 items-center">
-      {amount} <MdCurrencyRupee />
-    </p>
-    <div>
-      <button
-        className={`${buttonBgColor} px-4 py-2 rounded-full text-white font-bold shadow-md`}
-      >
-        {buttonStatus}
-      </button>
-    </div>
-  </div>
   );
 };
 
--- src/modules/homeChef/components/CardsView.js
+++ src/modules/homeChef/components/CardsView.js
@@ -28,12 +28,13 @@ const CardsView = () => {
   ];
 
   return (
-    <div className="bg-blue-100 rounded-md p-4 w-[100%] min-h-screen">
-      <div className="bg-white px-4 py-3 rounded-xl grid grid-cols-5 gap-5 text-start font-medium mb-3 shadow-md">
+    // <div className="bg-blue-100  rounded-md p-2 sm:p-8 w-[100%] min-h-screen">
+    <div className="bg-blue-100 rounded-md sm:p-4 w-full min-h-screen">
+      <div className="bg-white px-4 py-3 rounded-xl grid grid-cols-5  gap-5 text-start font-bold mb-3 shadow-md">
         <p>Bill id</p>
-        <p className="col-span-2">Order By</p>
-        <p>Amount</p>
-        <p>Action</p>
+        <p className="col-span-1  sm:col-span-2">Order By</p>
+        <p >Amount</p>
+        <p >Action</p>
       </div>
       {cardInfo &&
         cardInfo.map((cardInfo) => {
